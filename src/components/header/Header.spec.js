715e00b5fbec83f423701f5d3382da9d7480d71f
Krzysiek Kowalski <kowalski.k@outlook.com>
2017-05-03 11:26:53 +0100

    Added header component tests.

--- src/components/header/Header.spec.js
+++ src/components/header/Header.spec.js
@@ -21,4 +21,14 @@ describe('Components - Header', () => {
   it('should exist', () => {
     expect(wrapper).to.exist
   })
+
+  it('should render a logo element', () => {
+    expect(wrapper.find('.header__logo')).to.have.length(1)
+  })
+
+  it('should render a passed content', () => {
+    const content = <div className="header__content"/>
+    wrapper.setProps({content})
+    expect(wrapper.find('.header__content')).to.have.length(1)
+  })
 })
