e252ed5dfcd6aee9e924f381767e04cc0e9130ef
Krzysiek Kowalski <kowalski.k@outlook.com>
2017-05-05 09:53:32 +0100

    TRM249: Added desktop carousel styles. Fixed homepage hero headline styles.

--- src/components/home/partners/Partners.js
+++ src/components/home/partners/Partners.js
@@ -48,7 +48,7 @@ const Partners = props => {
 
   const slides = () => {
     let result = []
-    for (let i = 1; i < 5; i++) {
+    for (let i = 1; i <= 5; i++) {
       result.push(
         <div
           key={i}
@@ -64,7 +64,7 @@ const Partners = props => {
 
   return (
     <div className={_className}>
-      <div className='partners__slider-wrapper hidden-md-up'>
+      <div className='partners__carousel-wrapper hidden-md-up'>
         <Slider
           className='partners__carousel'
           infinite
@@ -80,7 +80,9 @@ const Partners = props => {
         </Slider>
       </div>
       <div className="container visible-md-up">
-        {slides()}
+        <div className="partners__carousel-desktop">
+          {slides()}
+        </div>
       </div>
     </div>
   )
