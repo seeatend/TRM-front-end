74805f95b81b49aa8bd76dd8a7153b6c747f89c7
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-06-03 22:22:22 +0100

    adding submit button

--- src/components/tiles/FeedSubmitTile/index.js
+++ src/components/tiles/FeedSubmitTile/index.js
@@ -19,6 +19,11 @@ import classNames from 'utils/classnames'
 import Accordion from 'components/accordion/BaseAccordion'
 
 /**
+ *  @module TextButton
+ */
+import TextButton from 'components/buttons/TextButton'
+
+/**
  *  @class
  *  @name FeedSubmitTile
  *  @extends {Component}
@@ -47,12 +52,21 @@ class FeedSubmitTile extends Component {
           <Accordion>
           </Accordion>
           <div className='feed-submit__bar row'>
-            <div className='col-xs-8'>
+            <div className='col-xs-8 align-middle'>
               <p className='micro feed-submit__bar__text'>
-                write a message
+                write a message...
               </p>
             </div>
-            <div className='col-xs-4 align-middle'></div>
+            <div className='col-xs-4 align-middle'>
+              <div className='row'>
+                <div className='col-xs-6 col-xs-push-6 col-md-4 col-md-push-8 no-padding'>
+                  <TextButton
+                    className='feed-submit__button'
+                    text='send'
+                    onClick={() => {}}/>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
