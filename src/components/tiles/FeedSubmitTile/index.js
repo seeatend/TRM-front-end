7df8ffa2b298092ca22a21dd3f158fade77568d6
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-06-04 00:51:52 +0100

    updated text button

--- src/components/tiles/FeedSubmitTile/index.js
+++ src/components/tiles/FeedSubmitTile/index.js
@@ -52,14 +52,14 @@ class FeedSubmitTile extends Component {
           <Accordion>
           </Accordion>
           <div className='feed-submit__bar row'>
-            <div className='col-xs-8 align-middle'>
+            <div className='col-xs-6 col-sm-8 align-middle'>
               <p className='micro feed-submit__bar__text'>
                 write a message...
               </p>
             </div>
-            <div className='col-xs-4 align-middle'>
+            <div className='col-xs-6 col-sm-4 align-middle'>
               <div className='row'>
-                <div className='col-xs-6 col-xs-push-6 col-md-4 col-md-push-8 no-padding'>
+                <div className='col-xs-5 col-xs-push-7 col-sm-5 col-sm-push-7 no-padding'>
                   <TextButton
                     className='feed-submit__button'
                     text='send'
