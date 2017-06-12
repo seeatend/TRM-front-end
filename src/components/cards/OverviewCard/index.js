245f61400768ac7751aac95384c1544b9c8c372a
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-06-12 16:10:35 +0100

    started on the overview card

---
+++ src/components/cards/OverviewCard/index.js
@@ -0,0 +1,100 @@
+/**
+ *  This card is used for the horse overview and syndicate overview pages.
+ */
+
+/**
+ *  @module React
+ */
+import React, { Component } from 'react'
+
+/**
+ *  @module PropTypes
+ */
+import PropTypes from 'prop-types'
+
+/**
+ *  @module Image
+ */
+import Image from 'components/image'
+
+/**
+ *  @module classNames
+ */
+import classNames from 'utils/classnames'
+
+import {
+  horseRaceImg
+} from 'assets/dummyassets'
+
+/**
+ *  @class
+ *  @name OverviewCard
+ *  @extends {Component}
+ */
+class OverviewCard extends Component {
+  /**
+   *  @constructor
+   */
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    const {
+      className,
+      modifier,
+      title
+    } = this.props
+
+    // Modified class names for overview card.
+    const modifiedClassNames = classNames('overview-card', className, modifier)
+
+    return (
+      <div className={modifiedClassNames}>
+        <Image
+          className='overview-card__bg'
+          imageSrc={horseRaceImg}
+          forceShow={true} />
+        <div className='overview-card__content'>
+          <div className='overview-card__card'>
+            <div className='overview-card__heading'>
+              <h3>
+                {title}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+/**
+ *  propTypes
+ *  @type {Object}
+ */
+OverviewCard.propTypes = {
+  className: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string)
+  ]),
+  modifier: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string)
+  ]),
+  title: PropTypes.string
+}
+
+/**
+ *  defaultProps
+ *  @type {Object}
+ */
+OverviewCard.defaultProps = {
+  className: '',
+  title: 'contrabrand horse'
+}
+
+/**
+ *  @module OverviewCard
+ */
+export default OverviewCard
