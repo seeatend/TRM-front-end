b2ed3a82327e5c65054e6b677237de978a35e545
Iliyan Yotov <iliyan@vitaminlondon.com>
2017-06-16 09:41:58 +0300

    Using unique mongoose id to make the submit work again

--- src/views/horseoverview/index.js
+++ src/views/horseoverview/index.js
@@ -114,7 +114,7 @@ export class HorseOverview extends Component {
 
   render () {
     const { showPopup, tileIndex } = this.state
-    const { data = {}, match } = this.props
+    const { data = {} } = this.props
     const { messages = [] } = data
 
     // Get the tile according to the passed in index.
@@ -131,7 +131,7 @@ export class HorseOverview extends Component {
             <div className='col-xs-12 col-sm-10 col-sm-push-1'>
               <SubmitPost
                 title='post an update to the horse'
-                horseId={match.params.name} />
+                horseId={data._id} />
             </div>
           </div>
         </div>
