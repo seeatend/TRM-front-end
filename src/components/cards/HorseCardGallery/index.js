24016c25efbec7b54a72497f83048f89003f7342
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-06-22 17:11:58 +0100

    adding in horse card gallery

---
+++ src/components/cards/HorseCardGallery/index.js
@@ -0,0 +1,115 @@
+/**
+ *  @module React
+ */
+import React, { PureComponent } from 'react'
+
+/**
+ *  @module PropTypes
+ */
+import PropTypes from 'prop-types'
+
+/**
+ *  @module HorseCard
+ */
+import HorseCard from 'components/cards/HorseCard'
+
+/**
+ *  @module constructStaticUrl
+ */
+import { constructStaticUrl } from 'utils/horseutils'
+
+/**
+ *  HorseCardGallery
+ *  @param  {Object} props
+ *  @return {PureComponent}
+ */
+class HorseCardGallery extends PureComponent {
+  constructor (props) {
+    super(props)
+
+    // Bind custom fn
+    this.renderChildren = this.renderChildren.bind(this)
+  }
+
+  /**
+   *  renderChildren
+   *  @param  {Object} entry
+   *  @return {Component}
+   */
+  renderChildren (entry) {
+    return (
+      <HorseCard
+        isActive={true}
+        src={constructStaticUrl(entry.thumbnailImage)}
+        title={entry.name}
+        color={entry.syndColor}
+        subtitle={`${entry.age}yo ${entry.gender}`} // Needs to have the STYLE too!
+        stats={[{
+          name: 'runs',
+          value: entry.runs
+        }, {
+          name: 'wins',
+          value: entry.wins
+        }, {
+          name: 'places',
+          value: entry.places
+        }, {
+          name: 'OR',
+          value: '-'
+        }]}
+        info={[{
+          name: 'Trainer name',
+          value: entry.trainer.name
+        }, {
+          name: 'Syndicate name',
+          value: entry.syndName
+        }, {
+          name: 'Initial cost/share',
+          value: '-'
+        }, {
+          name: 'Monthly cost/share',
+          value: '-'
+        }]}
+        extra={{
+          title: '5 of 20 shares available',
+          text: '*each share is equivalent to 5%'
+        }}
+        isMember={false}
+        bottomUrl={`/syndicate/${entry.syndSlug}`}
+        className='horse-card-gallery__card'/>
+    )
+  }
+
+  render () {
+    const {
+      data
+    } = this.props
+
+    return (
+      <div className='horse-card-gallery'>
+        {
+          data.map((entry, index) => {
+            return (
+              <div className='horse-card-gallery__item' key={index}>
+                {this.renderChildren(entry)}
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+HorseCardGallery.propTypes = {
+  data: PropTypes.array
+}
+
+HorseCardGallery.defaultProps = {
+  data: []
+}
+
+/**
+ *  @module HorseCardGallery
+ */
+export default HorseCardGallery
--- src/views/BrowseHorses/index.js
+++ src/views/BrowseHorses/index.js
@@ -16,6 +16,11 @@ import { BROWSE_HORSES as title } from 'data/titles'
  */
 import TitleHero from 'components/common/TitleHero'
 
+/**
+ *  @module HorseCardGallery
+ */
+import HorseCardGallery from 'components/cards/HorseCardGallery'
+
 export class BrowseHorses extends Component {
   constructor (props) {
     super(props)
@@ -26,6 +31,10 @@ export class BrowseHorses extends Component {
       <View title={title}>
         <div className='browse-horses'>
           <TitleHero/>
+          <div className='container'>
+            <HorseCardGallery
+              data={this.props.horses}/>
+          </div>
         </div>
       </View>
     )
@@ -39,6 +48,7 @@ export class BrowseHorses extends Component {
  *  @return {Object}
  */
 const mapStateToProps = (state, ownProps) => ({
+  horses: [{'_id': '594a734c371c882d01addd23', 'name': 'ELATION (IRE)', 'age': '10', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'Mark Johnston'}, 'featuredImage': '/uploads/horses/1498051404610/HERO_elation.jpg', 'thumbnailImage': '/uploads/horses/1498051404610/CARD_elation.jpg', 'slug': 'elation-(ire)', 'runs': 2, 'wins': 1, 'places': 0, 'shares': {'total': 26, 'owned': 6}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}, {'_id': '594a734c371c882d01addd24', 'name': 'CONTENTMENT', 'age': '3', 'gender': 'filly', 'owner': {'name': 'HIGHCLERE', 'slug': 'highclere'}, 'trainer': {'name': 'William Haggas'}, 'style': 'flat', 'featuredImage': '/uploads/horses/1498051404601/HERO_contentment.jpg', 'thumbnailImage': '/uploads/horses/1498051404601/CARD_contentment.jpg', 'slug': 'contentment', 'runs': 5, 'wins': 1, 'places': 2, 'shares': {'total': 26, 'owned': 1}}]
 })
 
 /**
