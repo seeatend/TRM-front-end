7974e81b4f61e22ca4c2117c1bfc56651fcca1dc
Iliyan Yotov <iliyan@vitaminlondon.com>
2017-06-26 11:40:40 +0300

    Small changes

--- src/components/horse/HorseAccordions/index.js
+++ src/components/horse/HorseAccordions/index.js
@@ -131,8 +131,7 @@ const results = {
     'IPS',
     'FS%',
     'TGIF',
-    'TFR',
-    'Notes'
+    'TFR'
   ],
   data: [
     [
@@ -154,7 +153,25 @@ const results = {
       '-',
       '-',
     ],
-    []
+    [
+      '15 May 07',
+      'Hun',
+      '1/13',
+      2.50,
+      'Chase',
+      'N',
+      '20.5f',
+      'Good',
+      '-',
+      'Felix de Giles (5)',
+      '8/11f',
+      '-',
+      '-',
+      '-',
+      '-',
+      '-',
+      '-',
+    ],
   ]
 }
 
@@ -214,50 +231,52 @@ const HorseAccordions = () => {
       </HorseAccordion>
       <HorseAccordion title='Racing plans'>
         <div className='horse-accordions__content container'>
-          <h1>
-            Race plans
-          </h1>
-          <Separator modifier='blue' />
-          <div>
-            <p>
-              The race plans for this filly will be determined by her level of ability shown at home and on the course in 2017.  On breeding she is sprint bred, but she has already shown signs of stamina and we expect she could be running over a mile as a 3YO.
-            </p>
-            <p>
-              With this filly having already run enough times to qualify for nursery handicaps, she will continue in this vein through 2017 - eventually into full handicap company, or if she proves to be good enough, possibly better company.  She showed at Chelmsford in November 2016 when finishing second on her handicap debut that she has ability and has progressed with every run to date.
-            </p>
-            <p>
-              This filly will be stabled in Lambourn, Berkshire throughout her 3YO career and will therefore be racing in the South at tracks such as Sandown, Windsor, Kempton, Lingfield, Newmarket, Ascot, Bath and others in the Midlands.
-            </p>
-            <p>
-              There are plenty of races available for her right from the start of the season.  We will assess this filly's progress and if Seamus believes she has the right level of ability she may get entries for some of the more prestigious handicap races in the racing calendar.
-            </p>
-            <p>
-              Your online Racehorse Manager will provide full details of all developments in terms of race planning as the season progresses.
-            </p>
-          </div>
-          <h1 className='horse-accordions__middle-title'>
-            Horse value
-          </h1>
-          <Separator modifier='blue' />
-          <div>
-            <p>
-              We expect to get 3-6 runs and possibly more from this racehorse in her share period, however, you should expect periods of no racing as a result of recuperation from injury or training setbacks. This share period starts April 1st 2017 and runs through the summer turf season, finishing on November 1st 2017.
-            </p>
-            <p>
-              During the season the horse may be rested or have time away from the track to recover and it is quite normal for a racehorse to have periods of 3-6 weeks rest between races. Young horses are particularly susceptible to sore shins, bone chips and growing pains, and can need a greater time to recover between races.
-            </p>
-            <p>
-              For this filly we offer the following guarantee:  If due to injury or retirement, this filly's season is cut short and will not race again, and she has not raced at least twice, we will replace her with a similar horse for the remainder of the 2017 turf season. Please note that we are unable to pay prizemoney on any replacements and the replacement will be a horse of our own choosing.
-            </p>
-            <p>
-              As a shareholder you must understand that we cannot guarantee your horses performance, a specific volume of runs (beyond our minimum guarantee) or that runs will be evenly spaced throughout the share period.
-            </p>
-            <p>
-              This is the chance you take when owning any racehorse and participating in this ownership experience.  If you will be disappointed with only 2-3 runs from your racehorse during the season, then please DO NOT PARTICIPATE IN THIS OR ANY OTHER RACEHORSE.
-            </p>
-            <p>
-              We believe in being crystal clear with our shareholders - any participation in racehorse ownership is high risk, and we are unable to make any refunds because of share periods which finish early due to injury or retirement - as training and livery fees for the horse still remain whether they are racing or not.  This is the chance all shareholders take, as we do ourselves, when participating in the ownership of a racehorse.
-            </p>
+          <div className='col-xs-12 col-md-7'>
+            <h1>
+              Race plans
+            </h1>
+            <Separator modifier='blue' />
+            <div>
+              <p>
+                The race plans for this filly will be determined by her level of ability shown at home and on the course in 2017.  On breeding she is sprint bred, but she has already shown signs of stamina and we expect she could be running over a mile as a 3YO.
+              </p>
+              <p>
+                With this filly having already run enough times to qualify for nursery handicaps, she will continue in this vein through 2017 - eventually into full handicap company, or if she proves to be good enough, possibly better company.  She showed at Chelmsford in November 2016 when finishing second on her handicap debut that she has ability and has progressed with every run to date.
+              </p>
+              <p>
+                This filly will be stabled in Lambourn, Berkshire throughout her 3YO career and will therefore be racing in the South at tracks such as Sandown, Windsor, Kempton, Lingfield, Newmarket, Ascot, Bath and others in the Midlands.
+              </p>
+              <p>
+                There are plenty of races available for her right from the start of the season.  We will assess this filly's progress and if Seamus believes she has the right level of ability she may get entries for some of the more prestigious handicap races in the racing calendar.
+              </p>
+              <p>
+                Your online Racehorse Manager will provide full details of all developments in terms of race planning as the season progresses.
+              </p>
+            </div>
+            <h1 className='horse-accordions__middle-title'>
+              Horse value
+            </h1>
+            <Separator modifier='blue' />
+            <div>
+              <p>
+                We expect to get 3-6 runs and possibly more from this racehorse in her share period, however, you should expect periods of no racing as a result of recuperation from injury or training setbacks. This share period starts April 1st 2017 and runs through the summer turf season, finishing on November 1st 2017.
+              </p>
+              <p>
+                During the season the horse may be rested or have time away from the track to recover and it is quite normal for a racehorse to have periods of 3-6 weeks rest between races. Young horses are particularly susceptible to sore shins, bone chips and growing pains, and can need a greater time to recover between races.
+              </p>
+              <p>
+                For this filly we offer the following guarantee:  If due to injury or retirement, this filly's season is cut short and will not race again, and she has not raced at least twice, we will replace her with a similar horse for the remainder of the 2017 turf season. Please note that we are unable to pay prizemoney on any replacements and the replacement will be a horse of our own choosing.
+              </p>
+              <p>
+                As a shareholder you must understand that we cannot guarantee your horses performance, a specific volume of runs (beyond our minimum guarantee) or that runs will be evenly spaced throughout the share period.
+              </p>
+              <p>
+                This is the chance you take when owning any racehorse and participating in this ownership experience.  If you will be disappointed with only 2-3 runs from your racehorse during the season, then please DO NOT PARTICIPATE IN THIS OR ANY OTHER RACEHORSE.
+              </p>
+              <p>
+                We believe in being crystal clear with our shareholders - any participation in racehorse ownership is high risk, and we are unable to make any refunds because of share periods which finish early due to injury or retirement - as training and livery fees for the horse still remain whether they are racing or not.  This is the chance all shareholders take, as we do ourselves, when participating in the ownership of a racehorse.
+              </p>
+            </div>
           </div>
         </div>
       </HorseAccordion>
