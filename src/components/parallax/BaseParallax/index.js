18e69dbbb3aecfd7ccd41f7eb28ad74adc5baa02
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-08-07 15:26:23 +0100

    updated parallax to extract getTop to domutils

--- src/components/parallax/BaseParallax/index.js
+++ src/components/parallax/BaseParallax/index.js
@@ -18,24 +18,10 @@ import { isInViewport } from 'utils/imageutils'
  */
 import throttle from 'utils/throttle'
 
-const getTop = (elem) => {
-  var box = elem.getBoundingClientRect()
-  var body = document.body
-  var docEl = document.documentElement
-
-  var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop
-  var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft
-
-  var clientTop = docEl.clientTop || body.clientTop || 0
-  var clientLeft = docEl.clientLeft || body.clientLeft || 0
-
-  var top = box.top + scrollTop - clientTop
-  var left = box.left + scrollLeft - clientLeft
-
-  return { top: Math.round(top), left: Math.round(left) }
-}
-
-let clamp = (x, low, high) => Math.min(Math.max(x, low), high)
+/**
+ *  @module getTop
+ */
+import { getTop } from 'utils/domutils'
 
 /**
  *  @class Parallax
