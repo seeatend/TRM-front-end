7510480eaf36c38328fdd2707d46f329bc7dc648
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-09-04 21:09:05 +0100

    adding in credit card component

--- src/views/Account/AccountPaymentMethods/index.js
+++ src/views/Account/AccountPaymentMethods/index.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react'
 
 import AccountAddCreditCardForm from 'containers/Account/AddCreditCard'
 
+import AccountCreditCard from 'components/account/AccountCreditCard'
+
 class AccountPaymentMethods extends PureComponent {
   constructor (props) {
     super(props)
@@ -28,6 +30,10 @@ class AccountPaymentMethods extends PureComponent {
             </div>
 
             <div className='account-payment-methods__section'>
+              <AccountAddCreditCardForm />
+            </div>
+
+            <div className='account-payment-methods__section'>
               <h4 className='capitalize'>
                 incoming
               </h4>
@@ -40,7 +46,27 @@ class AccountPaymentMethods extends PureComponent {
             </div>
 
             <div className='account-payment-methods__section'>
-              <AccountAddCreditCardForm />
+              <div className='row'>
+                <div className='col-sm-12 col-xs-12'>
+                  <AccountCreditCard
+                    cardType='visa debit'
+                    holderName='N DE ROZARIEUX'
+                    horseCount={1}
+                    cardNumber={'4658 **** **** ****'}
+                    expiry='12/20'
+                    postCode='BN21 2PP' />
+                </div>
+
+                <div className='col-sm-12 col-xs-12'>
+                  <AccountCreditCard
+                    cardType='visa credit'
+                    holderName='N DE ROZARIEUX'
+                    horseCount={2}
+                    cardNumber={'5046 **** **** ****'}
+                    expiry='10/19'
+                    postCode='SE13 3SS' />
+                </div>
+              </div>
             </div>
           </div>
         </div>
