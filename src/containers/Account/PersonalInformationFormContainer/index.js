7f594cc0c5ae26e6c8e3858014e1c4de47a3cd49
Nicholas de Rozarieux <nicholas@vitaminlondon.com>
2017-09-18 11:50:17 +0100

    updated personalinformationcontainer to use COmponent and not PureComponent

--- src/containers/Account/PersonalInformationFormContainer/index.js
+++ src/containers/Account/PersonalInformationFormContainer/index.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 
 import { connect } from 'react-redux'
 
@@ -12,7 +12,7 @@ import {
 
 import { personalInformationValidators } from 'utils/validation/PersonalInformation'
 
-class PersonalInformationFormContainer extends PureComponent {
+class PersonalInformationFormContainer extends Component {
   constructor (props) {
     super(props)
   }
